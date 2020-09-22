38bb6d22ac3f670d6954dc1fc08b3dcc98be03d0
rajneshsiwal <rajnesh.siwal@chargepoint.com>
2020-09-22 12:39:03 -0700

    Revert "[INFRA-761] Adding support for Aviatrix egress gateway"
    This reverts commit 81ee1d7673d8e300da8fda72526a8cc0f4a784b2.

--- main.tf
+++ main.tf
@@ -960,8 +960,7 @@ resource "aws_route" "private_nat_gateway" {
 
   route_table_id         = element(aws_route_table.private.*.id, count.index)
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = var.egress_gateway_network_interface_id == "" ? element(aws_nat_gateway.this.*.id, count.index) : null
-  network_interface_id   = var.egress_gateway_network_interface_id == "" ? null : var.egress_gateway_network_interface_id
+  nat_gateway_id         = element(aws_nat_gateway.this.*.id, count.index)
 
   timeouts {
     create = "5m"
